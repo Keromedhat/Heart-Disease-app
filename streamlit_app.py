1908dbcae950119c17be251a2d123e397317dc15
kerolse medhat <keromedhat873@gmail.com>
2025-10-01 01:10:21 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import pandas as pd
 st.title('Heart Disease app')
 st.info('This is app for Heart disease predicition')
 df=pd.read_csv("https://raw.githubusercontent.com/Keromedhat/Heart-Disease-app/refs/heads/master/cleaned%20Heart%20Disease%20.csv")
-df
+df.head(8)
